db54d2b7b10b7fcb2ee8d17db768b8142439e794
symoon94 <msy0128@gmail.com>
2020-06-24 12:56:50 +0900

    add argparse

---
+++ README.md
@@ -0,0 +1,9 @@
+## wav2png-librosa
+
+This converts wav or mp3 files to png files which represents a Mel spectrogram.
+
+## Usage
+
+set a path to your audio data
+
+    $ python create_img.py --wav_dir sample_data
--- create_img.py
+++ create_img.py
@@ -7,50 +7,64 @@ import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
 
-# mp3 to wav
-DIR_PATH = "./sample_data/"
-MP3_PATH = DIR_PATH + "*.mp3"
-
-mp3_list = glob.glob(MP3_PATH)
-for mp3 in mp3_list:
-    dst = mp3.replace('mp3', 'wav')
-    sound = AudioSegment.from_mp3(mp3)
-    sound.export(dst, format="wav")
-    
-    os.remove(mp3)
+import argparse
 
 
-# wav to png
-WAV_PATH = DIR_PATH + "*.wav"
-OUT_PATH = "./image/"
+def main(args):
+    # mp3 to wav
+    DIR_PATH = args.wav_dir
+    if not DIR_PATH.endswith("/"):
+        DIR_PATH += "/"
+    MP3_PATH = DIR_PATH + "*.mp3"
 
-if not os.path.isdir(OUT_PATH):
-	os.mkdir(OUT_PATH)
+    mp3_list = glob.glob(MP3_PATH)
+    for mp3 in mp3_list:
+        dst = mp3.replace('mp3', 'wav')
+        sound = AudioSegment.from_mp3(mp3)
+        sound.export(dst, format="wav")
+        
+        os.remove(mp3)
 
-wav_list = glob.glob(WAV_PATH)
-i = 0
-for wav in wav_list:
-    dst = OUT_PATH + wav.lstrip(DIR_PATH).replace('wav', 'png')
-    y, sr = librosa.load(wav)
 
-    # melspectrogram
-    D = np.abs(librosa.stft(y))**2
-    S = librosa.feature.melspectrogram(S=D, sr=sr)
+    # wav to png
+    WAV_PATH = DIR_PATH + "*.wav"
+    OUT_PATH = "./image/"
 
-    fig = plt.figure(figsize=(10, 4))
-    S_dB = librosa.power_to_db(S, ref=np.max)
-    librosa.display.specshow(S_dB, x_axis='time',
-                            y_axis='mel', sr=sr,
-                            fmax=8000)
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Mel-frequency spectrogram')
-    plt.tight_layout()
-    # plt.show()
+    if not os.path.isdir(OUT_PATH):
+        os.mkdir(OUT_PATH)
 
-    fig.savefig(dst)
+    wav_list = glob.glob(WAV_PATH)
+    i = 0
+    for wav in wav_list:
+        dst = OUT_PATH + wav.lstrip(DIR_PATH).replace('wav', 'png')
+        y, sr = librosa.load(wav)
 
-    i +=1
-    if i % 10 == 0:
-        print(f"complete {i}th image...")
+        # melspectrogram
+        D = np.abs(librosa.stft(y))**2
+        S = librosa.feature.melspectrogram(S=D, sr=sr)
+
+        fig = plt.figure(figsize=(10, 4))
+        S_dB = librosa.power_to_db(S, ref=np.max)
+        librosa.display.specshow(S_dB, x_axis='time',
+                                y_axis='mel', sr=sr,
+                                fmax=8000)
+        plt.colorbar(format='%+2.0f dB')
+        plt.title('Mel-frequency spectrogram')
+        plt.tight_layout()
+        # plt.show()
+
+        fig.savefig(dst)
+
+        i +=1
+        if i % 10 == 0:
+            print(f"complete {i}th image...")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--wav_dir', type=str, default = '', help = "set a directory path of your audio data", required = True)
+
+    args = parser.parse_args()
+    main(args)
 
 
